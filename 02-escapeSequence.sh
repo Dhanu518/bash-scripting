1006ad1ecc04f9dd7abd94a0f393bd00912ad680
Dhanu518 <jakkamdhanu0518@gmail.com>
2023-09-10 21:31:38 +0530

    copied from Manu's Git

--- 02-escapeSequence.sh
+++ 02-escapeSequence.sh
@@ -8,6 +8,20 @@ echo Welcome To Bach Scripting
 # This is how we can execute the scripts
 # $ bash scriptName.sh  or  sh scriptName.sh  or  ./scriptName.sh
 
+#  \n : This indicates a new line 
+# \t  : This gives a tab space 
+#  Manoj\nDevOps
+#  Manoj
+#  DevOps
+
 echo Line1
 echo Line2
 echo Line3
+
+echo -e "Line5\nLine6"
+# Whenever you're using special characters , always enclose them in DOUBLE QUOTES
+# " "  : Double Quotes 
+# ' '  : Single Quotes
+
+echo -e "Cloud\tDevOps\tare\tBooming"
+echo -e "We\tAre\tLearning\tCloudDevOps \n\tThis is my batch55"
